e16fba9148cc4b591c9e4d13c0c31e84352edc93
aknaus1 <aknausc@gmail.com>
2022-03-18 00:44:39 -0700

    .

--- jetson/guirecieve.py
+++ jetson/guirecieve.py
@@ -6,34 +6,34 @@ if __name__ == '__main__':
     args = sys.argv[1:]
 
     if args[0] == "mission":
-        sc.mission(int(args[1]), args[2], args[3], args[4], args[5], args[6], args[7], args[9], args[10])
+        sc.mission(int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]), int(args[6]), int(args[7]), int(args[9]), int(args[10]))
     elif args[0] == "setThrust":
-            sc.setThrust(args[1])
+            sc.setThrust(int(args[1]))
     elif args[0] == "setRudder":
-        sc.setRudder(args[1])
+        sc.setRudder(int(args[1]))
     elif args[0] == "setHeading":
         if(len(args) == 2):
-            sc.setHeading(args[1])
+            sc.setHeading(int(args[1]))
         else:
-            sc.setHeading(args[1], args[2])
+            sc.setHeading(int(args[1]), int(args[2]))
     elif args[0] == "rudderSensorRequest":
-        sc.rudderSensorRequest(args[1])
+        sc.rudderSensorRequest(int(args[1]))
     elif args[0] == "setPitch":
         if(len(args) == 2):
-            sc.setPitch(args[1])
+            sc.setPitch(int(args[1]))
         else:
-            sc.setPitch(args[1], args[2])
+            sc.setPitch(int(args[1]), int(args[2]))
     elif args[0] == "setDepth":
         if(len(args) == 2):
-            sc.setPitch(args[1])
+            sc.setPitch(int(args[1]))
         else:
-            sc.setDepth(args[1], args[2], args[3])
+            sc.setDepth(int(args[1]), int(args[2]), int(args[3]))
     elif args[0] == "setStepper":
-        sc.setStepper(args[1])
+        sc.setStepper(int(args[1]))
     elif args[0] == "pitchSensorRequest":
-        sc.pitchSensorRequest(args[1])
+        sc.pitchSensorRequest(int(args[1]))
     elif args[0] == "startDataCollection":
-        sc.startDataCollection(args[1], args[2])
+        sc.startDataCollection(int(args[1]), int(args[2]))
     elif args[0] == "stopDataCollection":
         sc.stopDataCollection()
     else:
