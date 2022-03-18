a61a35b82dd7b399a6fe40a928f22018697202b8
aknaus1 <aknausc@gmail.com>
2022-03-18 00:42:36 -0700

    .

--- jetson/guirecieve.py
+++ jetson/guirecieve.py
@@ -6,28 +6,28 @@ if __name__ == '__main__':
     args = sys.argv[1:]
 
     if args[0] == "mission":
-        sc.mission(args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[9], args[10])
+        sc.mission(int(args[1]), args[2], args[3], args[4], args[5], args[6], args[7], args[9], args[10])
     elif args[0] == "setThrust":
             sc.setThrust(args[1])
     elif args[0] == "setRudder":
         sc.setRudder(args[1])
     elif args[0] == "setHeading":
-        if args[2] != None:
-            sc.setHeading(args[1], args[2])
-        else:
+        if(len(args) == 2):
             sc.setHeading(args[1])
+        else:
+            sc.setHeading(args[1], args[2])
     elif args[0] == "rudderSensorRequest":
         sc.rudderSensorRequest(args[1])
     elif args[0] == "setPitch":
-        if args[2] != None:
+        if(len(args) == 2):
+            sc.setPitch(args[1])
+        else:
             sc.setPitch(args[1], args[2])
-        else:
-            sc.setPitch(args[1])
     elif args[0] == "setDepth":
-        if args[2] != None and args[3] != None:
-            sc.setDepth(args[1], args[2], args[3])
-        else:
+        if(len(args) == 2):
             sc.setPitch(args[1])
+        else:
+            sc.setDepth(args[1], args[2], args[3])
     elif args[0] == "setStepper":
         sc.setStepper(args[1])
     elif args[0] == "pitchSensorRequest":
