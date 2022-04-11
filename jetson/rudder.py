00c234e3cb58bebbbf60e53ca57af15f80b1fc8c
aknaus1 <aknausc@gmail.com>
2022-04-10 17:11:11 -0700

    .

--- jetson/rudder.py
+++ jetson/rudder.py
@@ -17,7 +17,7 @@ class RudderControl:
 
     comms = CANBUS_COMMS()
 
-    def __init__(self, in_lock, out_lock):
+    def __init__(self, in_lock = threading.Lock(), out_lock = threading.Lock()):
         self.in_lock = in_lock
         self.out_lock = out_lock
         return
