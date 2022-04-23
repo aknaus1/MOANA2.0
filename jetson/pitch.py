75148b091332384278ac37f444f81586c04c017e
aknaus1 <aknausc@gmail.com>
2022-04-22 18:05:26 -0700

    4-22

--- jetson/pitch.py
+++ jetson/pitch.py
@@ -41,12 +41,14 @@ class PitchControl:
 
         print("Send Position: " + str(pos))
 
+        decimal = abs(pos - floor(pos))
+
         data = []
         data.append(5)  # Write pitch ID
         data.append(2)  # Write stepper command
         data.append(0 if pos < 0 else 1)
         data.append(round(abs(pos)))  # Write position
-        data.append(round((abs(pos) - abs(floor(pos)))*100))  # Write position
+        data.append(abs(pos - floor(pos)))  # Write position
 
         self.comms.writeToBus(data) # Write to CAN
         self.cur_pos = pos
