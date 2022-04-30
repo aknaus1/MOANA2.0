ecac2b5c8cf1e4e9bb466f1af3bfe4cc278d923b
Silas Kidd-Myers <31936603+silaskiddmyers@users.noreply.github.com>
2022-04-29 20:02:05 -0700

    big push- copde is godly now watch out jeff bezos

--- arduino/pitch/pitch.ino
+++ arduino/pitch/pitch.ino
@@ -4,6 +4,8 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
 
 #define MESSAGE_ID 5       // Message ID
 #define MESSAGE_PROTOCOL 1 // CAN protocol (0: CAN 2.0A, 1: CAN 2.0B)
@@ -13,6 +15,11 @@
 
 #define STEP_CONST .0021
 #define IDLE 69
+#define MAINTAIN_DEPTH 2
+
+
+#define BNO055_SAMPLERATE_DELAY_MS 10
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 const int dirPin = 8;
 const int stepPin = 7;
@@ -28,7 +35,15 @@ uint8_t Buffer[8] = {};
 void calibrate();
 void CANsend(int ID, int sensor);
 void nudgeStepper();
+float getPitch();
 int sensorRequest = 0;
+
+float pitch_kp = 4 / 3;
+float depth_kp = 3;
+float pitch_cmd = 0;
+float depth_cmd = 0;
+float cur_depth = 0;
+
 
 float stepsToX = 0; // centimeters
 float distance = 0; // meters
@@ -83,6 +98,16 @@ void setup()
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
 
+
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1)
+      ;
+  }
+  bno.setExtCrystalUse(true);
+
   Serial.println("Set pins");
   Serial.println("Initialized sensors");
   calibrate(); // runs calibration
@@ -92,11 +117,19 @@ void loop()//main loop, refreshes every
 {
   //Serial.println("head of loop");
   CANin();
-  Serial.print("State:");
-  Serial.println(type);//type is changed in CANin, it's the second byte of the message and dictates what the board does once it receives a message
+//  Serial.print("State:");
+//  Serial.println(type);//type is changed in CANin, it's the second byte of the message and dictates what the board does once it receives a message
 
   //switch statements may make more sense here but in testing we found that the boards were not powerful enough to handle it
-  if (type == 1)
+  if (type == 6) //depth command
+  {
+    double newPitch = (depth_cmd - cur_depth) * depth_kp + MAINTAIN_DEPTH;
+    Serial.print("New Pitch: ");
+    Serial.println(newPitch);
+    double changePos = (newPitch - getPitch()) * pitch_kp;
+    changeSliderPosition(changePos);
+  }
+  else if (type == 1)
   {
     changeSliderPosition(sliderChange);
   }
@@ -112,6 +145,22 @@ void loop()//main loop, refreshes every
   {
     calibrate(); // runs calibration
   }
+  else if (type == 7) //pitch command
+  {
+    double changePos = (pitch_cmd - getPitch()) * pitch_kp;
+    changeSliderPosition(changePos);
+  }
+
+}
+
+float getPitch() // reads pitch from sensor
+{
+  sensors_event_t event;
+  bno.getEvent(&event);
+  float zpos = event.orientation.z;
+  Serial.print("Pitch: ");
+  Serial.println(zpos);
+  return zpos;
 }
 
 void nudgeStepper() //moves stepper a little bit in the direction it's been set
@@ -132,15 +181,15 @@ void setSliderPosition(float dist) //sets slider position based on an input -16 
 }
 
 void changeSliderPosition(double change) { //changes slider position based on an input in centimeters- will stop at end if input would be too far
+  if((currentLocation >= 7620 && change > 0) || (currentLocation <= -7620 && change < 0)) return;
   //set direction of stepper motor
   Serial.print("Change: ");
   Serial.println(change);
   stepsToX = change / STEP_CONST;//calculate how much slider will move in a unit that is nice for the stepper
   stepsToX > 0 ? digitalWrite(dirPin, HIGH) : digitalWrite(dirPin, LOW);//set stepper direction
 
-  Serial.print("Steps To X: ");
-  Serial.println(stepsToX);
-  Serial.println("About to start slider movement");
+//  Serial.print("Steps To X: ");
+//  Serial.println(stepsToX);
   if (stepsToX + currentLocation > 7620) //if input would move too far forward
   {
     stepsToX = 7620 - currentLocation;//set new input to be the end on the side it was told to go
@@ -230,25 +279,68 @@ void CANin()
     return;
   }
   int dir = 0, angle = 0;
-  type = Msg.pt_data[MESSAGE_TYPE]; // determines whether message indicates a change in pitch or change in depth
-  if (type == 1)
+  type = Msg.pt_data[MESSAGE_TYPE]; // determines type of message
+  if (type == 0) //receive depth from depth board, type 0 in main loop will call depth control
+  {
+    cur_depth = Msg.pt_data[MESSAGE_TYPE + 1] + Msg.pt_data[MESSAGE_TYPE + 2] / 100;
+
+  }
+  else if (type == 1)//stepper change
   {
     sliderChange = Msg.pt_data[MESSAGE_TYPE + 1] == 1 // if direction is positive
                    ? (Msg.pt_data[MESSAGE_TYPE + 2] + (Msg.pt_data[MESSAGE_TYPE + 3] / 100)) //distance = positive of input
                    : -(Msg.pt_data[MESSAGE_TYPE + 2] + (Msg.pt_data[MESSAGE_TYPE + 3] / 100));//else distance = negative of input
   }
-  else if (type == 2)
+  else if (type == 2)//stepper position
   {
     distance = Msg.pt_data[MESSAGE_TYPE + 1] == 1 // if direction is positive
                ? (Msg.pt_data[MESSAGE_TYPE + 2] + (Msg.pt_data[MESSAGE_TYPE + 3] / 100)) //distance = positive of input
                : -(Msg.pt_data[MESSAGE_TYPE + 2] + (Msg.pt_data[MESSAGE_TYPE + 3] / 100));//else distance = negative of input
   }
-  else if (type == 3)
+  else if (type == 3) //sensor request
   {
     sensorRequest = Msg.pt_data[MESSAGE_TYPE + 1];
   }
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  else if (type == 5) // kp command
+  {
+    pitch_kp = Msg.pt_data[MESSAGE_TYPE + 1] + Msg.pt_data[MESSAGE_TYPE + 2] / 100;
+    depth_kp = Msg.pt_data[MESSAGE_TYPE + 3] + Msg.pt_data[MESSAGE_TYPE + 4] / 100;
+  }
+  else if (type == 6) //depth command
+  {
+    depth_cmd = Msg.pt_data[MESSAGE_TYPE + 1];
+
+    //send depth sensor request
+    clearBuffer(&Buffer[0]);
+    Msg.id.ext = MESSAGE_ID; // Set message ID
+    Buffer[0] = DEPTH_TEMP;
+    Buffer[1] = 3;
+    Buffer[2] = DEPTH;
+    Msg.cmd = CMD_TX_DATA;
+    while (can_cmd(&Msg) != CAN_CMD_ACCEPTED);
+    while (can_get_status(&Msg) == CAN_STATUS_NOT_COMPLETED);
+
+
+    do {//repeatedly wait for depth sensor response
+      clearBuffer( & Buffer[0]);
+      Msg.cmd = CMD_RX_DATA; // Wait for the command to be accepted by the controller
+      while (can_cmd( & Msg) != CAN_CMD_ACCEPTED);
+      while (can_get_status( & Msg) == CAN_STATUS_NOT_COMPLETED);
+    } while ((Msg.pt_data[0] != DEPTH_PITCH) && (Msg.pt_data[MESSAGE_TYPE] != 0));
+    
+    cur_depth = Msg.pt_data[MESSAGE_TYPE + 1] + Msg.pt_data[MESSAGE_TYPE + 2] / 100;
+    Serial.print("Depth: ");
+    Serial.println(cur_depth);
+
+    Serial.print("Desired depth: ");
+    Serial.println(depth_cmd);
+  }
+  else if (type == 7) //pitch command
+  {
+    pitch_cmd = Msg.pt_data[MESSAGE_TYPE + 1] == 1 // if direction is positive
+                ? (Msg.pt_data[MESSAGE_TYPE + 2] + (Msg.pt_data[MESSAGE_TYPE + 3] / 100)) //distance = positive of input
+                : -(Msg.pt_data[MESSAGE_TYPE + 2] + (Msg.pt_data[MESSAGE_TYPE + 3] / 100));
+  }
   clearBuffer( & Buffer[0]);
 }
 
@@ -277,8 +369,8 @@ void convert(float testValue) // converts a float or double into an array that c
 
 void sliderDone()
 {
-  Serial.print("Slider done: current Location is ");
-  Serial.println(currentLocation);
+//  Serial.print("Slider done: current Location is ");
+//  Serial.println(currentLocation);
   type = IDLE;
   //CANsend(JETSON, SLIDER);
 }
