c0f89430ad6fbbbf506b8fdc45bd38f02aaee537
aknaus1 <aknausc@gmail.com>
2022-04-30 18:44:36 -0700

    4-30

--- jetson/canbus_comms.py
+++ jetson/canbus_comms.py
@@ -56,7 +56,7 @@ class CANBUS_COMMS:
                     logger.info(block[1:])
                     print("Download complete")
                     return
-                logger.info(block)
+                logger.info(block + '\n')
             except Exception as error_message:
                 print(f"Error reading from bus: {error_message}")
                 return [0,0,0,0,0,0,0,0]
